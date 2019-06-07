bf446a7d88c479f4f96f6ee9841c757b75118011
Mike Snyder <mikesnyder88@gmail.com>
2019-06-07 14:40:02 -0500

    fixed seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,38 @@
+USE horsedb;
+-- ************************************** `User`
+CREATE TABLE `User`
+(
+ `UserId`   integer NOT NULL AUTO_INCREMENT ,
+ `Password` varchar(45) NOT NULL ,
+ `UserName` varchar(45) NOT NULL ,
+ `Created`  timestamp NOT NULL ,
+
+PRIMARY KEY (`UserId`)
+);
+-- ************************************** `Horse`
+
+CREATE TABLE `Horse`
+(
+ `HorseId`      integer NOT NULL AUTO_INCREMENT ,
+ `HorseName`    varchar(45) NOT NULL ,
+ `UserId`       integer NOT NULL ,
+ `Age`          integer NOT NULL ,
+ `HorseCreated` timestamp NOT NULL ,
+
+PRIMARY KEY (`HorseId`),
+KEY `fkIdx_24` (`UserId`),
+CONSTRAINT `FK_24` FOREIGN KEY `fkIdx_24` (`UserId`) REFERENCES `User` (`UserId`)
+);
+-- ************************************** `HorseStats`
+
+CREATE TABLE `HorseStats`
+(
+ `HorseId`      integer NOT NULL ,
+ `TopSpeed`     integer NOT NULL ,
+ `Acceleration` integer NOT NULL ,
+ `Reliability`  integer NOT NULL ,
+ `Endurance`    integer NOT NULL ,
+
+KEY `fkIdx_44` (`HorseId`),
+CONSTRAINT `FK_44` FOREIGN KEY `fkIdx_44` (`HorseId`) REFERENCES `Horse` (`HorseId`)
+);

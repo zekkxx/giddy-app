88b2e5c5fa2e49b79013e2b3af79c6de1b9cc4df
Mike Snyder <mikesnyder88@gmail.com>
2019-06-10 14:58:28 -0500

    removed UpdatedAt column and made auto generated columns snake case.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,40 @@
 DROP DATABASE IF EXISTS horsedb;
 CREATE DATABASE horsedb;
+
+CREATE TABLE IF NOT EXISTS `Users`
+    (
+    `id` INTEGER NOT NULL auto_increment , 
+    `password` VARCHAR(255), 
+    `username` VARCHAR(255) NOT NULL, 
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY(`id`)
+    )
+    ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Horses`
+    (
+    `id` INTEGER NOT NULL auto_increment , 
+    `horse_name` VARCHAR(255), 
+    `age` INTEGER, 
+    `created_at` DATETIME NOT NULL, 
+    `user_id` INTEGER, 
+    PRIMARY KEY(`id`), 
+    FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`) 
+    ON DELETE SET NULL ON UPDATE CASCADE
+    ) 
+    ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Stats`
+    (
+    `id` INTEGER NOT NULL auto_increment , 
+    `speed` INTEGER, 
+    `acceleration` INTEGER, 
+    `reliability` INTEGER,
+    `endurance` INTEGER, 
+    `created_at` DATETIME NOT NULL, 
+    `horse_id` INTEGER, 
+    PRIMARY KEY(`id`), 
+    FOREIGN KEY(`horse_id`) REFERENCES `Horses`(`id`) 
+    ON DELETE CASCADE ON UPDATE CASCADE
+    ) 
+    ENGINE=InnoDB;
--- models/stat.js
+++ models/stat.js
@@ -2,39 +2,47 @@
 /* eslint-disable camelcase */
 /* eslint-disable linebreak-style */
 module.exports = function(sequelize, DataTypes) {
-  var Stat = sequelize.define("Stat", {
-    speed: {
-      type: DataTypes.INTEGER,
-      allowNUll: false,
-      validate: {
-        len: [0, 5],
-        isInt: true
+  var Stat = sequelize.define(
+    "Stat",
+    {
+      speed: {
+        type: DataTypes.INTEGER,
+        allowNUll: false,
+        validate: {
+          len: [0, 5],
+          isInt: true
+        }
+      },
+      acceleration: {
+        type: DataTypes.INTEGER,
+        allowNUll: false,
+        validate: {
+          len: [0, 5],
+          isInt: true
+        }
+      },
+      reliability: {
+        type: DataTypes.INTEGER,
+        allowNUll: false,
+        validate: {
+          len: [0, 5],
+          isInt: true
+        }
+      },
+      endurance: {
+        type: DataTypes.INTEGER,
+        allowNUll: false,
+        validate: {
+          len: [0, 5],
+          isInt: true
+        }
       }
     },
-    acceleration: {
-      type: DataTypes.INTEGER,
-      allowNUll: false,
-      validate: {
-        len: [0, 5],
-        isInt: true
-      }
-    },
-    reliability: {
-      type: DataTypes.INTEGER,
-      allowNUll: false,
-      validate: {
-        len: [0, 5],
-        isInt: true
-      }
-    },
-    endurance: {
-      type: DataTypes.INTEGER,
-      allowNUll: false,
-      validate: {
-        len: [0, 5],
-        isInt: true
-      }
+    {
+      timestamps: true,
+      updatedAt: false,
+      underscored: true
     }
-  });
+  );
   return Stat;
 };

310cc1ff1076165b9d08ebcc802d49eeeda17cad
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-06-10 16:32:17 -0500

    Made various changes regarding the login/registration process.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,20 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+
+  app.post("/register", function(req, res){
+    db.User.create(req.body).then(function(response){
+      console.log(response);
+      res.redirect("/login");
     });
   });
 
-/*  app.post("/api/horses", function(req, res) {
+  app.post("/api/horses", function(req, res) {
     db.Horse.create(req.body).then(function(dbHorse) {
       db.Stat.create(req.body).then(function(req, res) { 
         res.json(dbHorse);
       });
       // db.Stats.create. inside the .then statement I will include sthe res.json
     });
-  });*/
+  });
 };

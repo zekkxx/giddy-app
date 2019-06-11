c5c3dee1fa9660547e5deba5eea053b50395ba11
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-06-11 13:09:20 -0500

    Temporarily disabled res.json from horses post route for testing.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,15 +9,16 @@ module.exports = function(app) {
     });
   });
 
-  app.post("/api/horses", function(req, res) {
+  app.post("/api/horses", function(req/*, res*/) {
     db.Horse.create(req.body).then(function(dbHorse) {
       console.log(dbHorse.dataValues.id);
       db.Stat.create(req.body).then(function(dbStat) { 
         console.log(dbHorse);
         console.log(dbStat);
-        res.json(dbHorse);
+        //res.json(dbHorse);
       });
       // db.Stats.create. inside the .then statement I will include sthe res.json
+      //Currently posts json object, should redirect to appropriate page.
     });
   });
 };

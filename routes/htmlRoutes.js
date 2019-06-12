fade06951e8bf67d46f4e6db5f27bb8b6db648e2
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-06-12 09:39:42 -0500

    Added functionality for horse/owner/:id

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,14 @@
 var db = require("../models");
+
+var ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn;
+var checkLogin = function(req, res, next){
+  if(req.user){
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
+
 module.exports = function(app) {
   
   app.get("/addhorse", function(req, res) {
@@ -9,15 +19,21 @@ module.exports = function(app) {
   });
 
   app.get("/horse/owner/:id", function(req, res) {
-    // get all horses with owner Id 2
-    db.Horse.findAll({
+    db.User.findOne({
       include:[db.Horse],
-      where: {UserId: req.params.id}
-    }).then(function(dbHorses) {
-      res.json(dbHorses);
+      where: {id: req.params.id},
+      attributes: {
+        exclude: ["password"] //user has the password in it, let's filter that out...for...security.......yes, I know we saved the password in db in plaintext...get off my back
+      }
+    }).then(function(dbUserHorses) {
+      res.render("horsebyowner", {
+        user: dbUserHorses.username,
+        horses: dbUserHorses.Horses
+      });
+      //res.json(dbUserHorses);
     });
     // .then do res.render and pass in the data I recieve
-    res.render("horsebyowner", {id:1, horse:"hi"});
+    //res.render("horsebyowner", {id:1, horse:"hi"});
   });
 
   app.get("/login", function(req, res) {
